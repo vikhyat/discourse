31b95aba7689a3e9d37c12d8743b9d12f3b42802
Vikhyat Korrapati <vikhyatk@gmail.com>
2013-09-22 20:42:50 +0530

    Temporarily add back the application view.

---
+++ app/assets/javascripts/discourse/views/application_view.js
@@ -0,0 +1,14 @@
+/**
+  This view handles rendering of the core application template
+
+  @class ApplicationView
+  @extends Discourse.View
+  @namespace Discourse
+  @module Discourse
+**/
+Discourse.ApplicationView = Discourse.View.extend({
+  templateName: 'application'
+});
+
+
+
